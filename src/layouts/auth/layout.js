b6f124e414fac2a474968fcad36c1560d2a4f6d3
Manuel <neycariom@gmail.com>
2023-06-25 18:27:10 +0100

    Pagina de login actualizada, registerPage removido

--- src/layouts/auth/layout.js
+++ src/layouts/auth/layout.js
@@ -6,106 +6,107 @@ import { Logo } from 'src/components/logo';
 // TODO: Change subtitle text
 
 export const Layout = (props) => {
-  const { children } = props;
+	const { children } = props;
 
-  return (
-    <Box
-      component="main"
-      sx={{
-        display: 'flex',
-        flex: '1 1 auto'
-      }}
-    >
-      <Grid
-        container
-        sx={{ flex: '1 1 auto' }}
-      >
-        <Grid
-          xs={12}
-          lg={6}
-          sx={{
-            backgroundColor: 'background.paper',
-            display: 'flex',
-            flexDirection: 'column',
-            position: 'relative'
-          }}
-        >
-          <Box
-            component="header"
-            sx={{
-              left: 0,
-              p: 3,
-              position: 'fixed',
-              top: 0,
-              width: '100%'
-            }}
-          >
-            <Box
-              component={NextLink}
-              href="/"
-              sx={{
-                display: 'inline-flex',
-                height: 32,
-                width: 32
-              }}
-            >
-              <Logo />
-            </Box>
-          </Box>
-          {children}
-        </Grid>
-        <Grid
-          xs={12}
-          lg={6}
-          sx={{
-            alignItems: 'center',
-            background: 'radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)',
-            color: 'white',
-            display: 'flex',
-            justifyContent: 'center',
-            '& img': {
-              maxWidth: '100%'
-            }
-          }}
-        >
-          <Box sx={{ p: 3 }}>
-            <Typography
-              align="center"
-              color="inherit"
-              sx={{
-                fontSize: '24px',
-                lineHeight: '32px',
-                mb: 1
-              }}
-              variant="h1"
-            >
-              Welcome to{' '}
-              <Box
-                component="a"
-                sx={{ color: '#15B79E' }}
-                target="_blank"
-              >
-                Devias Kit
-              </Box>
-            </Typography>
-            <Typography
-              align="center"
-              sx={{ mb: 3 }}
-              variant="subtitle1"
-            >
-              A professional kit that comes with ready-to-use MUI components.
-            </Typography>
-            <img
-              alt=""
-              src="/assets/auth-illustration.svg"
-            />
-          </Box>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+	return (
+		<Box
+			component="main"
+			sx={{
+				display: 'flex',
+				flex: '1 1 auto'
+			}}
+		>
+			<Grid
+				container
+				sx={{ flex: '1 1 auto' }}
+			>
+				<Grid
+					xs={12}
+					lg={6}
+					sx={{
+						backgroundColor: 'background.paper',
+						display: 'flex',
+						flexDirection: 'column',
+						position: 'relative'
+					}}
+				>
+					<Box
+						component="header"
+						sx={{
+							left: 0,
+							p: 3,
+							position: 'fixed',
+							top: 0,
+							width: '100%'
+						}}
+					>
+						<Box
+							component={NextLink}
+							href="/"
+							sx={{
+								display: 'inline-flex',
+								height: 64,
+								width: 64
+							}}
+						>
+							<Logo />
+						</Box>
+					</Box>
+					{children}
+				</Grid>
+				<Grid
+					xs={12}
+					lg={6}
+					sx={{
+						alignItems: 'center',
+						background: 'radial-gradient(50% 50% at 50% 50%, #7cc4e8 0%, #0484cc 100%)',
+						color: 'white',
+						display: 'flex',
+						justifyContent: 'center',
+						'& img': {
+							maxWidth: '100%'
+						}
+					}}
+				>
+					<Box sx={{ p: 3 }}>
+						<Typography
+							align="center"
+							color="inherit"
+							sx={{
+								fontSize: '24px',
+								lineHeight: '32px',
+								mb: 1
+							}}
+							variant="h1"
+						>
+							Welcome to{' '}
+							<Box
+								component="a"
+								sx={{ color: '#dc242c' }}
+								target="_blank"
+							>
+								Localiza
+							</Box>
+						</Typography>
+						<Typography
+							align="center"
+							sx={{ mb: 3 }}
+							variant="subtitle1"
+							color="inherit"
+						>
+							Um aplicativo para localizar ATM com dinheiro e Bombas de combustível com combustível
+						</Typography>
+						<img
+							alt=""
+							src="/assets/auth-illustration.svg"
+						/>
+					</Box>
+				</Grid>
+			</Grid>
+		</Box>
+	);
 };
 
 Layout.prototypes = {
-  children: PropTypes.node
+	children: PropTypes.node
 };
